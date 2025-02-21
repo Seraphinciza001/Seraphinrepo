d61ff4929b5906688226646209b5bf8bd25f2ee7
Seraphinciza001 <seraphinciza659@gmail.com>
2025-02-21 15:55:22 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
-  author: 'Bot',
+  packname: 'DUPENTAVICE BOT',
+  author: 'Seraphin',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botOwner: 'Seraphin', // Your name
+  ownerNumber: '243896472013', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
